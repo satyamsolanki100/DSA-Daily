b8a7744a170c83391f8a77cd25463de121fcba91
Satyam Kumar Solanki <satyamkumarsolanki318@gmail.com>
2026-05-21 01:52:59 +0530

    TrappingRainWater  Problems

---
+++ Stack_Queue/TrappingRainWater.java
@@ -0,0 +1,44 @@
+public class TrappingRainWater {
+
+    public static int trap(int[] height) {
+
+        int left = 0, right = height.length - 1;
+        int leftMax = 0, rightMax = 0;
+        int water = 0;
+
+        while (left <= right) {
+
+            if (height[left] < height[right]) {
+
+                if (height[left] >= leftMax) {
+                    leftMax = height[left];
+                } else {
+                    water += leftMax - height[left];
+                }
+
+                left++;
+
+            } else {
+
+                if (height[right] >= rightMax) {
+                    rightMax = height[right];
+                } else {
+                    water += rightMax - height[right];
+                }
+
+                right--;
+            }
+        }
+
+        return water;
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr1 = {0,1,0,2,1,0,1,3,2,1,2,1};
+        System.out.println(trap(arr1)); // 6
+
+        int[] arr2 = {4,2,0,3,2,5};
+        System.out.println(trap(arr2)); // 9
+    }
+}
