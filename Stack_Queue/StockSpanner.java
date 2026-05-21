7af845a3169ad1fc0180c4a4d75f2915c4825978
Satyam Kumar Solanki <satyamkumarsolanki318@gmail.com>
2026-05-22 04:10:40 +0530

    StockSpanner  Problems

---
+++ Stack_Queue/StockSpanner.java
@@ -0,0 +1,34 @@
+// File: StockSpanner.java
+
+import java.util.*;
+
+public class StockSpanner {
+
+    private Stack<int[]> stack;
+
+    public StockSpanner() {
+        stack = new Stack<>();
+    }
+
+    public int next(int price) {
+        int span = 1;
+
+        while (!stack.isEmpty() && stack.peek()[0] <= price) {
+            span += stack.pop()[1];
+        }
+
+        stack.push(new int[]{price, span});
+        return span;
+    }
+
+    public static void main(String[] args) {
+        StockSpanner stockSpanner = new StockSpanner();
+        System.out.println(stockSpanner.next(100)); // 1
+        System.out.println(stockSpanner.next(80));  // 1
+        System.out.println(stockSpanner.next(60));  // 1
+        System.out.println(stockSpanner.next(70));  // 2
+        System.out.println(stockSpanner.next(60));  // 1
+        System.out.println(stockSpanner.next(75));  // 4
+        System.out.println(stockSpanner.next(85));  // 6
+    }
+}
