d579de89201c6f4dc972c9330722255f1e48a10e
Satyam Kumar Solanki <satyamkumarsolanki318@gmail.com>
2026-05-29 01:05:57 +0530

    LemonadeChange  questions

---
+++ Greedy Algorithm/LemonadeChange.java
@@ -0,0 +1,44 @@
+// File: LemonadeChange.java
+
+public class LemonadeChange {
+
+    public static boolean lemonadeChange(int[] bills) {
+
+        int five = 0, ten = 0;
+
+        for (int bill : bills) {
+
+            if (bill == 5) {
+                five++;
+            } 
+            else if (bill == 10) {
+                if (five == 0) return false;
+                five--;
+                ten++;
+            } 
+            else { // bill == 20
+                if (ten > 0 && five > 0) {
+                    ten--;
+                    five--;
+                } 
+                else if (five >= 3) {
+                    five -= 3;
+                } 
+                else {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        int[] bills1 = {5,5,5,10,20};
+        System.out.println(lemonadeChange(bills1)); // true
+
+        int[] bills2 = {5,5,10,10,20};
+        System.out.println(lemonadeChange(bills2)); // false
+    }
+}
