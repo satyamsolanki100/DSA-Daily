6eb28b98470a3032009b5021cd4993e93d5563c4
Satyam Kumar Solanki <satyamkumarsolanki318@gmail.com>
2026-06-04 01:32:56 +0530

    MaxDepth  questions

---
+++ Binary Trees/MaxDepth.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+class TreeNode {
+    int val;
+    TreeNode left, right;
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+}
+
+public class MaxDepth {
+
+    public static int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+
+        return 1 + Math.max(left, right);
+    }
+
+    public static void main(String[] args) {
+
+        /*
+              3
+             / \
+            9  20
+               / \
+              15  7
+        */
+
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+
+        System.out.println(maxDepth(root)); // 3
+    }
+}
