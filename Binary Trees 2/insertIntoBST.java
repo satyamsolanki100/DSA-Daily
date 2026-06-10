9372258aaf22edaf4bf899d8992a3681d6c562d5
Satyam Kumar Solanki <satyamkumarsolanki318@gmail.com>
2026-06-10 14:12:42 +0530

    insertIntoBST  questions

---
+++ Binary Trees 2/insertIntoBST.java
@@ -0,0 +1,56 @@
+public class InsertBST {
+
+    static class TreeNode {
+        int val;
+        TreeNode left, right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public static TreeNode insertIntoBST(TreeNode root, int val) {
+
+        if (root == null) {
+            return new TreeNode(val);
+        }
+
+        if (val < root.val) {
+            root.left = insertIntoBST(root.left, val);
+        } else {
+            root.right = insertIntoBST(root.right, val);
+        }
+
+        return root;
+    }
+
+    // 🔍 Test
+    public static void main(String[] args) {
+
+        /*
+                4
+               / \
+              2   7
+             / \
+            1   3
+        */
+
+        TreeNode root = new TreeNode(4);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(7);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(3);
+
+        root = insertIntoBST(root, 5);
+
+        inorder(root);
+    }
+
+    // print inorder
+    public static void inorder(TreeNode root) {
+        if (root == null) return;
+        inorder(root.left);
+        System.out.print(root.val + " ");
+        inorder(root.right);
+    }
+}
